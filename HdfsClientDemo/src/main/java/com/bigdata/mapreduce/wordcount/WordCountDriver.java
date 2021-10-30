3fa04e3479afa5d34c2bd1c3c458d868d3bd39df
zhigang <fdrgdf@foxmail.com>
2021-10-31 00:05:16 +0800

    mapreduce

--- HdfsClientDemo/src/main/java/com/bigdata/mapreduce/wordcount/WordCountDriver.java
+++ HdfsClientDemo/src/main/java/com/bigdata/mapreduce/wordcount/WordCountDriver.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -25,6 +26,17 @@ public class WordCountDriver {
         // 3 关联Mapper和Reducer的jar
         job.setMapperClass(WordCountMapper.class);
         job.setReducerClass(WordCountReducer.class);
+
+        // 如果不设置InputFormat，它默认用的是TextInputFormat.class
+        job.setInputFormatClass(CombineTextInputFormat.class);
+
+//虚拟存储切片最大值设置4m   运行如果为3个切片 number of splits:3
+        CombineTextInputFormat.setMaxInputSplitSize(job, 4194304);
+
+        //虚拟存储切片最大值设置20m   运行如果为1个切片
+        //number of splits:1
+       // CombineTextInputFormat.setMaxInputSplitSize(job, 20971520);
+
 
         // 4 设置Mapper输出的kv类型
         job.setMapOutputKeyClass(Text.class);
