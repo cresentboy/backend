3ab7815bd3349881e87acbd5cbcda8bd2c3d5d4c
zhigang <fdrgdf@foxmail.com>
2021-11-12 09:24:09 +0800

    flink_scala

---
+++ bigdata/flink/src/main/flink/com/practice/bigdata/flink_scala_1/wc/WordCount.scala
@@ -0,0 +1,26 @@
+package com.practice.bigdata.flink_scala_1.wc
+
+import org.apache.flink.api.scala._
+
+
+// 批处理 word count，DataSet API
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    // 创建一个批处理的执行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    // 从文件中读取数据
+    val inputFilePath: String = "datas/a.txt"
+    val inputDataSet: DataSet[String] = env.readTextFile(inputFilePath)
+
+    // 对 DataSet进行转换操作
+    val resultDataSet: DataSet[(String, Int)] = inputDataSet
+      .flatMap(_.split(" "))    // 把每一行数据打散，分词
+      .map( (_, 1) )    // 转换成二元组
+      .groupBy(0)    // 把二元组中下标为0的第一个元素，作为key分组
+      .sum(1)    // 把当前key中所有的第二个字段sum起来，得到count
+
+    // 打印输出
+    resultDataSet.print()
+  }
+}
---
+++ bigdata/flink/src/main/flink/com/practice/bigdata/flink_scala_2/apitest/tabletest/Example.scala
@@ -0,0 +1,48 @@
+package com.practice.bigdata.flink_scala_2.apitest.tabletest
+
+
+import com.practice.bigdata.flink_scala_2.apitest.SensorReading
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.Table
+import org.apache.flink.table.api.scala._
+
+
+object Example {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    // 读取数据
+    val inputPath = "D:\\Projects\\BigData\\FlinkTutorial\\src\\main\\resources\\sensor.txt"
+    val inputStream = env.readTextFile(inputPath)
+    //    val inputStream = env.socketTextStream("localhost", 7777)
+
+    // 先转换成样例类类型（简单转换操作）
+    val dataStream = inputStream
+      .map(data => {
+        val arr = data.split(",")
+        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+      })
+
+    // 首先创建表执行环境
+    val tableEnv = StreamTableEnvironment.create(env)
+
+    // 基于流创建一张表
+    val dataTable: Table = tableEnv.fromDataStream(dataStream)
+
+    // 调用table api进行转换
+    val resultTable = dataTable
+      .select("id, temperature")
+      .filter("id == 'sensor_1'")
+
+    // 直接用sql实现
+    tableEnv.createTemporaryView("dataTable", dataTable)
+    val sql: String = "select id, temperature from dataTable where id = 'sensor_1'"
+    val resultSqlTable = tableEnv.sqlQuery(sql)
+
+    resultTable.toAppendStream[(String, Double)].print("result")
+    resultSqlTable.toAppendStream[(String, Double)].print("result sql")
+
+    env.execute("table api example")
+  }
+}
